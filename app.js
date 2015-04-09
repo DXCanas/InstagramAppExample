bb62f256d97c759f044b42133c8266fe27c37d93
David C <davidxcanas@gmail.com>
2015-04-08 23:21:45 -0700

    Finished the assignment

--- app.js
+++ app.js
@@ -9,25 +9,22 @@ var hashtag = require('./routes/hashtag');
 var index = require('./routes/index');
 //database setup
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGOHQ_URL || 'mongodb://localhost/instagramexample');
-
+mongoose.connect(process.env.MONGOLAB_URL || 'mongodb://localhost/instagramexample');
 //Configures the Template engine
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.bodyParser());
-
 //routes
 app.get('/', index.view);
 app.get('/hashtag', function (req, res) {
-	res.render('hashtag');
+res.render('hashtag');
 })
 app.post('/hashtag', hashtag.getHashtag);
 app.post('/save', hashtag.saveFavorites);
-app.post('/delete', index.deleteImage);
 //set environment ports and start application
 app.set('port', process.env.PORT || 3000);
 http.createServer(app).listen(app.get('port'), function(){
-	console.log('Express server listening on port ' + app.get('port'));
+console.log('Express server listening on port ' + app.get('port'));
 });
